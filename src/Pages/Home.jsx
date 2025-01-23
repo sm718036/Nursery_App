678482ca8044eb95e9ecbe278476e6fa50f70c3c
Sherry <sm718036@gmail.com>
2025-01-23 10:04:03 +0500

    Changes home styles to be mobile responsive

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,10 +5,10 @@ function Home() {
   return (
     <main className="h-screen bg-green-700 text-white flex items-center flex-col justify-center gap-4">
       <h1 className="text-4xl font-bold text-center">Welcome to PlantEase</h1>
-      <p className="text-gray-300">
+      <p className="text-gray-300 text-center">
         Your one-stop destination for all things green and beautiful.
       </p>
-      <p className="text-gray-300 text-justify w-[50%]">
+      <p className="text-gray-300 text-center w-[90%] md:w-[60%] lg:w-[60%]">
         At PlantEase, we are passionate about bringing nature closer to you by
         offering a diverse selection of houseplants, succulents, and outdoor
         plants. Our mission is to make plant shopping easy and accessible,
